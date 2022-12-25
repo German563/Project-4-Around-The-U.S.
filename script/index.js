0f361acae889394e2b84a2549ae8a10fc1ea4957
German563 <73666252+German563@users.noreply.github.com>
2022-12-25 14:05:36 +0200

    first commit

--- script/index.js
+++ script/index.js
@@ -1,13 +1,13 @@
-const modalWindow = document.querySelector(".popup");
-const openWindiowButton = document.querySelector(".gallery__pencil");
-const closeWindowButton = document.querySelector(".popup__close");
+var modalWindow = document.querySelector(".popup");
+var openWindiowButton = document.querySelector(".gallery__pencil");
+var closeWindowButton = document.querySelector(".popup__close");
 var saveModalWindow = document.querySelector(".popup__button");
 var saveButton = document.querySelector(".popup__button");
-let nameInput = document.forms.profile.elements.name;
-let titleInput = document.forms.profile.elements.title;
-let galleryName = document.querySelector('.gallery__header');
-let galleryTitle = document.querySelector('.gallery__subtext');
-// let heartButton = document.querySelectorAll("img[src='./images/heart.svg']");
+var nameInput = document.forms.profile.elements.name;
+var titleInput = document.forms.profile.elements.title;
+var galleryName = document.querySelector('.gallery__header');
+var galleryTitle = document.querySelector('.gallery__subtext');
+var heartButton = document.querySelectorAll("img[src='./images/heart.svg']");
 
 function toggleModalWindow() {
 modalWindow.classList.toggle("popup_opened");
