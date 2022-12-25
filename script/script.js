c94dc45f1fe3acb670ff0e27d994bd20f7b14568
German563 <73666252+German563@users.noreply.github.com>
2022-12-25 13:52:36 +0200

    first commit

--- script/script.js
+++ script/script.js
@@ -1,1 +1,26 @@
-console.log("banana");
+const modalWindow = document.querySelector(".popup");
+const openWindiowButton = document.querySelector(".gallery__pencil");
+const closeWindowButton = document.querySelector(".popup__close");
+var saveModalWindow = document.querySelector(".popup__button");
+var saveButton = document.querySelector(".popup__button")
+let nameInput = document.forms.profile.elements.name;
+let titleInput = document.forms.profile.elements.title;
+let galleryName = document.querySelector('.gallery__header');
+let galleryTitle = document.querySelector('.gallery__subtext');
+// let heartButton = document.querySelectorAll("img[src='./images/heart.svg']");
+
+function toggleModalWindow() {
+modalWindow.classList.toggle("popup_opened");
+}
+openWindiowButton.addEventListener("click", toggleModalWindow);
+closeWindowButton.addEventListener("click", toggleModalWindow);
+
+
+saveButton.addEventListener ("click", function(){
+    galleryName.textContent = nameInput.value;
+    galleryTitle.textContent = titleInput.value;
+    toggleModalWindow();
+});
+
+
+ 
