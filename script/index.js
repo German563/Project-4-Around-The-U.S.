277418d6dd59c038fc258bf8e9203145deccdfe3
German563 <73666252+German563@users.noreply.github.com>
2022-12-26 20:18:05 +0200

    JS redone

--- script/index.js
+++ script/index.js
@@ -3,34 +3,32 @@ const openWindiowButton = document.querySelector(".gallery__pencil");
 const closeWindowButton = document.querySelector(".popup__close");
 const saveModalWindow = document.querySelector(".popup__button");
 const saveButton = document.querySelector(".popup__button");
-let nameInput = document.forms.profile.elements.name;
-let titleInput = document.forms.profile.elements.title;
+const nameInput = document.forms.profile.elements.name;
+const titleInput = document.forms.profile.elements.title;
 const galleryName = document.querySelector('.gallery__header');
 const galleryTitle = document.querySelector('.gallery__subtext');
 
+function fillProfileForm() {
+    nameInput.value = galleryName.textContent;
+    titleInput.value = galleryTitle.textContent;
+}
+
 function toggleModalWindow() {
     modalWindow.classList.toggle("popup_opened");
-    function fillProfileForm() {
-        nameInput.value = galleryName.textContent;
-        titleInput.value = galleryTitle.textContent;
-      }
-      
-      function openEditProfileModal() {
-        fillProfileForm();
-        toggleModalWindow();
-       } 
+}
+function openEditProfileModal() {
+    fillProfileForm();
+    toggleModalWindow();
 }
 openWindiowButton.addEventListener("click", toggleModalWindow);
 closeWindowButton.addEventListener("click", toggleModalWindow);
 
 
-saveButton.addEventListener("click", function () {
+modalWindow.addEventListener("submit", function () {
     galleryName.textContent = nameInput.value;
     galleryTitle.textContent = titleInput.value;
     toggleModalWindow();
 });
-  
-  openWindiowButton.addEventListener("click", openEditProfileModal);
-  openWindiowButton.addEventListener("submit", openEditProfileModal);
+
 
 
