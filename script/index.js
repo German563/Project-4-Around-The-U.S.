a37ecdf40d31f1bc9b7d90db54997ca77a520567
German563 <73666252+German563@users.noreply.github.com>
2022-12-26 11:40:15 +0200

    JS redone

--- script/index.js
+++ script/index.js
@@ -1,16 +1,24 @@
-var modalWindow = document.querySelector(".popup");
-var openWindiowButton = document.querySelector(".gallery__pencil");
-var closeWindowButton = document.querySelector(".popup__close");
-var saveModalWindow = document.querySelector(".popup__button");
-var saveButton = document.querySelector(".popup__button");
-var nameInput = document.forms.profile.elements.name;
-var titleInput = document.forms.profile.elements.title;
-var galleryName = document.querySelector('.gallery__header');
-var galleryTitle = document.querySelector('.gallery__subtext');
-var heartButton = document.querySelectorAll("img[src='./images/heart.svg']");
+const modalWindow = document.querySelector(".popup");
+const openWindiowButton = document.querySelector(".gallery__pencil");
+const closeWindowButton = document.querySelector(".popup__close");
+const saveModalWindow = document.querySelector(".popup__button");
+const saveButton = document.querySelector(".popup__button");
+let nameInput = document.forms.profile.elements.name;
+let titleInput = document.forms.profile.elements.title;
+const galleryName = document.querySelector('.gallery__header');
+const galleryTitle = document.querySelector('.gallery__subtext');
 
 function toggleModalWindow() {
     modalWindow.classList.toggle("popup_opened");
+    function fillProfileForm() {
+        nameInput.value = galleryName.textContent;
+        titleInput.value = galleryTitle.textContent;
+      }
+      
+      function openEditProfileModal() {
+        fillProfileForm();
+        toggleModalWindow();
+       } 
 }
 openWindiowButton.addEventListener("click", toggleModalWindow);
 closeWindowButton.addEventListener("click", toggleModalWindow);
@@ -21,6 +29,8 @@ saveButton.addEventListener("click", function () {
     galleryTitle.textContent = titleInput.value;
     toggleModalWindow();
 });
-
+  
+  openWindiowButton.addEventListener("click", openEditProfileModal);
+  openWindiowButton.addEventListener("submit", openEditProfileModal);
 
 
