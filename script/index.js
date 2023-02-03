be145c7993e454b04d398cc6c9b09f8eba18539c
German563 <73666252+German563@users.noreply.github.com>
2023-02-03 22:46:35 +0200

    JS new additions

--- script/index.js
+++ script/index.js
@@ -1,28 +1,28 @@
 const initialCards = [
   {
     name: "Yosemite Valley",
-    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
   },
   {
     name: "Lake Louise",
-    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
   },
   {
     name: "Bald Mountains",
-    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
   },
   {
     name: "Latemar",
-    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
   },
   {
     name: "Vanoise National Park",
-    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
   },
   {
     name: "Lago di Braies",
-    link: "https://code.s3.yandex.net/web-code/lago.jpg"
-  }
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
 ];
 
 const profileModal = document.querySelector(".popup_type_edit-profile");
@@ -33,70 +33,81 @@ const saveProfileModal = profileModal.querySelector(".popup__button");
 // inputs for person
 const nameInput = document.forms.profile.elements.name;
 const titleInput = document.forms.profile.elements.title;
-// inputs for new card 
-const cardLinkTitle = document.querySelector("#nameImg");
-const cardLinkInput = document.querySelector("#titleURL");
-const galleryName = document.querySelector('.gallery__header');
-const galleryTitle = document.querySelector('.gallery__subtext');
+// inputs for new card
+const cardLinkTitle = document.querySelector("#form__place");
+const cardLinkInput = document.querySelector("#type_card-url");
+const galleryName = document.querySelector(".gallery__header");
+const galleryTitle = document.querySelector(".gallery__subtext");
 const galleryLink = document.querySelector(".popup__input_type_card-link");
 const createButton = document.querySelector(".button_active");
 const newCardLinkTitle = document.querySelector(".popup__title-foto");
 const placesList = document.querySelector(".card__area");
 // All forms
-const formEdit = document.getElementById('form__edit');
-const formPlace = document.getElementById('form__place');
+const formEdit = document.getElementById("form__edit");
+const formPlace = document.getElementById("form__place");
 // Close buttons
 const deleteBigImg = document.querySelector("#closeButtonFoto");
-
+const closeOverlay = document.querySelector(".popup__background");
 
 function fillProfileForm() {
   nameInput.value = galleryName.textContent;
   titleInput.value = galleryTitle.textContent;
 }
 function openModal(modal) {
+  openCloseOverlay();
   modal.classList.add("popup_opened");
-
-
+  window.addEventListener("keydown", closeModalByEscape);
+  window.addEventListener("mousedown", closeModalByEscape);
+}
+function openCloseOverlay() {
+  closeOverlay.classList.add("popup__background_opened");
+}
+function closeCloseOverlay() {
+  closeOverlay.classList.remove("popup__background_opened");
 }
 function closeModal(modal) {
   modal.classList.remove("popup_opened");
+  closeCloseOverlay();
+  window.removeEventListener("keydown", closeModalByEscape);
+  window.removeEventListener("mousedown", closeModalByEscape);
 }
-window.addEventListener("keydown", function (event) {
+function closeAllModals() {
+  closeCardModal();
+  closeEdit();
+  closeBigImg();
+  closeCloseOverlay();
+}
+
+function closeModalByEscape(event) {
   if (event.key === "Escape") {
-    closeCardModal();
-    closeEdit();
-    closeBigImg()
+    closeAllModals();
   }
-});
-window.addEventListener("click", function (event) {
-  if (event.key === "Escape") {
-    closeCardModal();
-    closeEdit();
-    closeBigImg()
-  }
-});
+}
+
+closeOverlay.addEventListener("click", closeAllModals);
 
 function openEdit() {
   fillProfileForm();
   checkInitialFormValidity(profileModal.querySelector("form"), pageSettings);
+  openCloseOverlay();
   openModal(profileModal);
 }
 function closeEdit() {
-  closeModal(profileModal)
+  closeModal(profileModal);
+  closeCloseOverlay();
+  window.removeEventListener("keydown", closeModalByEscape);
 }
-
 
 // reset form functions
 function resetPlaceForm() {
-  formPlace.reset()
+  formPlace.reset();
 }
 function resetEditForm() {
-  formEdit.reset()
+  formEdit.reset();
 }
 
 openEditButton.addEventListener("click", openEdit);
 closeProfileModalButton.addEventListener("click", closeEdit);
-
 
 profileModal.addEventListener("submit", function () {
   galleryName.textContent = nameInput.value;
@@ -104,33 +115,31 @@ profileModal.addEventListener("submit", function () {
   resetEditForm();
   closeEdit();
 });
-// add card 
+// add card
 
 const galleryButton = document.querySelector(".gallery__button");
 const closeButtonNewPlace = document.querySelector("#closeButtonNewPlace");
 
-
 function openCardModal() {
   checkInitialFormValidity(popupAddCard.querySelector("form"), pageSettings);
-  openModal(popupAddCard)
+  openModal(popupAddCard);
 }
 function closeCardModal() {
-  closeModal(popupAddCard)
+  closeModal(popupAddCard);
 }
-galleryButton.addEventListener("click", openCardModal)
+galleryButton.addEventListener("click", openCardModal);
 closeButtonNewPlace.addEventListener("click", closeCardModal);
-
 
 //new card creation//
 // new place modal
 
-const cardTemplate = document.querySelector("#card-template").content.querySelector(".card__gallery");
+const cardTemplate = document
+  .querySelector("#card-template")
+  .content.querySelector(".card__gallery");
 const cardElement = cardTemplate.cloneNode(true);
 const listContainer = document.querySelector(".card__area");
 
-
 const createNewCard = (props) => {
-
   const localName = props.name;
   const localLink = props.link;
   const localCardTemplate = props.cardTemplate;
@@ -138,37 +147,45 @@ const createNewCard = (props) => {
   const cardLinkTitle = clonedCard.querySelector(".card__ellipsis");
   const cardLinkInput = clonedCard.querySelector(".card__image");
 
-
   cardLinkTitle.textContent = localName;
   cardLinkInput.src = localLink;
   cardLinkInput.alt = "picture";
   clonedCard.querySelector("#heart").addEventListener("click", changeHeart);
   function changeHeart() {
-    const chosenHeart = clonedCard.querySelector("#heart")
-    if (chosenHeart.getAttribute("src") === "./images/heart.svg") {
-      chosenHeart.setAttribute("src", "./images/black-hearth.svg");
+    const chosenHeart = clonedCard.querySelector("#heart");
+    if (
+      chosenHeart.getAttribute("style") ===
+      "background-image: url(../images/heart.svg)"
+    ) {
+      chosenHeart.setAttribute(
+        "style",
+        "background-image: url(../images/black-hearth.svg)"
+      );
     } else {
-      chosenHeart.setAttribute("src", "./images/heart.svg");
+      chosenHeart.setAttribute(
+        "style",
+        "background-image: url(../images/heart.svg)"
+      );
     }
   }
 
-
-
-
-  clonedCard.querySelector(".card__delete-button").addEventListener("click", (e) => {
-    e.target.closest(".card__gallery").remove();
-  });
+  clonedCard
+    .querySelector(".card__delete-button")
+    .addEventListener("click", (e) => {
+      e.target.closest(".card__gallery").remove();
+    });
 
   function openBigImg(e) {
     openModal(bigImg);
     bigImg.style = "background-image: url(" + cardLinkInput.src + " )";
-    newCardLinkTitle.textContent = clonedCard.querySelector(".card__ellipsis").textContent;
+    newCardLinkTitle.textContent =
+      clonedCard.querySelector(".card__ellipsis").textContent;
   }
   cardLinkInput.addEventListener("click", openBigImg);
 
   return clonedCard;
 };
-const bigImg = document.querySelector('.popup_type_foto');
+const bigImg = document.querySelector(".popup_type_foto");
 function closeBigImg() {
   closeModal(bigImg);
 }
@@ -177,18 +194,15 @@ deleteBigImg.addEventListener("click", closeBigImg);
 // create new card
 
 formPlace.addEventListener("submit", (e) => {
-  const cardTemplate = document.querySelector("#card-template").content.querySelector(".card__gallery");
   const clonedCard = createNewCard({
-    name: document.getElementById("nameImg").value,
-    link: document.getElementById("titleURL").value,
-    cardTemplate: cardTemplate
+    name: document.getElementById("type_card-name").value,
+    link: document.getElementById("type_card-url").value,
+    cardTemplate: cardTemplate,
   });
   placesList.prepend(clonedCard);
   closeCardModal(popupAddCard);
   resetPlaceForm();
 });
-
-
 
 const populateCards = () => {
   initialCards.forEach((item) => {
@@ -202,4 +216,3 @@ const populateCards = () => {
   fillProfileForm(profileModal);
 };
 populateCards();
-
--- script/validate.js
+++ script/validate.js
@@ -1,74 +1,85 @@
 const pageSettings = {
-    formSelector: ".popup__form",
-    inputSelector: ".popup__input",
-    submitButtonSelector: ".popup__button",
-    inactiveButtonClass: "popup__button_disabled",
-    inputErrorClass: "popup__input_type_error",
-    errorClass: "popup__error_visible"
-  }
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible",
+};
 
 function showInputError(formElement, inputElement, settings) {
-const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
 
-inputElement.classList.add(settings.inputErrorClass);
-errorElement.textContent = inputElement.validationMessage;
-errorElement.classList.add(settings.errorClass);
+  inputElement.classList.add(settings.inputErrorClass);
+  errorElement.textContent = inputElement.validationMessage;
+  errorElement.classList.add(settings.errorClass);
 }
 
-function hideInputError(formElement, inputElement,settings) {
-const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+function hideInputError(formElement, inputElement, settings) {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
 
-inputElement.classList.remove(settings.inputErrorClass);
-errorElement.textContent = "";
-errorElement.classList.remove(settings.errorClass);
+  inputElement.classList.remove(settings.inputErrorClass);
+  errorElement.textContent = "";
+  errorElement.classList.remove(settings.errorClass);
 }
-function checkInputValidity(formElement, inputElement, settings) {
-    if(inputElement.validity.valid) {
-        hideInputError(formElement, inputElement, settings);
-    } else {
-        showInputError(formElement, inputElement, settings);
-    }
+function toggleInputError(formElement, inputElement, settings) {
+  if (inputElement.validity.valid) {
+    hideInputError(formElement, inputElement, settings);
+  } else {
+    showInputError(formElement, inputElement, settings);
+  }
 }
 
 const toggleButtonState = (inputElements, buttonElement, settings) => {
-    const hasInvalidInput = inputElements.some(inputElement => !inputElement.validity.valid);
+  const hasInvalidInput = inputElements.some(
+    (inputElement) => !inputElement.validity.valid
+  );
 
-    if(hasInvalidInput) {
-        buttonElement.classList.add(settings.inactiveButtonClass);
-    } else {
-        buttonElement.classList.remove(settings.inactiveButtonClass);
-    }
+  if (hasInvalidInput) {
+    buttonElement.classList.add(settings.inactiveButtonClass);
+    buttonElement.setAttribute("disabled", "");
+  } else {
+    buttonElement.classList.remove(settings.inactiveButtonClass);
+    buttonElement.removeAttribute("disabled", "");
+  }
 };
 
 function setEventListeners(formElement, settings) {
-const inputElements = [...formElement.querySelectorAll(settings.inputSelector)];
-const buttonElement = formElement.querySelector(settings.submitButtonSelector);
+  const inputElements = [
+    ...formElement.querySelectorAll(settings.inputSelector),
+  ];
+  const buttonElement = formElement.querySelector(
+    settings.submitButtonSelector
+  );
 
-inputElements.forEach(inputElement => {
+  inputElements.forEach((inputElement) => {
     inputElement.addEventListener("input", () => {
-        checkInputValidity(formElement, inputElement, settings);
-        toggleButtonState(inputElements, buttonElement, settings);
-    })
-})
-toggleButtonState(inputElements, buttonElement, settings);
+      toggleInputError(formElement, inputElement, settings);
+      toggleButtonState(inputElements, buttonElement, settings);
+    });
+  });
 }
 
 function enableValidation(settings) {
-    const forms = document.querySelectorAll(settings.formSelector);
+  const forms = document.querySelectorAll(settings.formSelector);
 
-    forms.forEach(formElement => {
-        formElement.addEventListener("submit", e => {
-            e.preventDefault();
-        });
-        setEventListeners(formElement, settings);
-    })
+  forms.forEach((formElement) => {
+    formElement.addEventListener("submit", (e) => {
+      e.preventDefault();
+    });
+    setEventListeners(formElement, settings);
+  });
 }
 
 function checkInitialFormValidity(formElement, settings) {
-const inputElements = [...formElement.querySelectorAll(settings.inputSelector)];
-const buttonElement = formElement.querySelector(settings.submitButtonSelector);
+  const inputElements = [
+    ...formElement.querySelectorAll(settings.inputSelector),
+  ];
+  const buttonElement = formElement.querySelector(
+    settings.submitButtonSelector
+  );
 
-toggleButtonState(inputElements, buttonElement, settings);
+  toggleButtonState(inputElements, buttonElement, settings);
 }
 
 enableValidation(pageSettings);
