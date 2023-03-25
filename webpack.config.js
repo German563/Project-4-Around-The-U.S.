fb41cd6634e18423d4554c47f31327448311a4a9
German563 <73666252+German563@users.noreply.github.com>
2023-03-26 00:40:56 +0300

    webpack old

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,59 @@
-const path = require('path');
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
 module.exports = {
   devtool: 'inline-source-map',
   entry: {
-    main: './src/pages/index.js'
+    main: "./src/pages/index.js"
   },
+
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
-    publicPath: ''
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: ""
   },
   target: ['web', 'es5'],
   stats: { children: true },
-  mode: 'development',
+  mode: "development",
   devServer: {
-    static: path.resolve(__dirname, './dist'),
+    static: path.resolve(__dirname, "./dist"),
     compress: true,
     port: 8080,
     open: true
   },
   module: {
-    rules: [ // this is an array of rules
-      // add an object containing rules for Babel to it
+    rules: [
       {
-        // a regular expression that searches for all js files
         test: /\.js$/,
-        // all files must be processed by babel-loader
         loader: "babel-loader",
-        // exclude the node_modules folder, we don't need to process files in it
         exclude: "/node_modules/"
       },
       {
-        test: /\.css$/,
+    test: /\.css$/,
         use: [
           MiniCssExtractPlugin.loader,
           {
             loader: "css-loader",
             options: {
-              importLoaders: 1
-            }
+             importLoaders: 1
+             }
           },
           "postcss-loader"
-        ],  
+        ],
       },
       {
-        // add the rule for processing files
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
         type: "asset/resource"
-      },
+      }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html", // path to our index.html file
-      basePath: "/"
+      template: "./src/index.html"
     }),
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin(),// connect the plugin for merging CSS files
-    new NodePolyfillPlugin()
-  ]
-  
+    new MiniCssExtractPlugin()
+  ],
 }
